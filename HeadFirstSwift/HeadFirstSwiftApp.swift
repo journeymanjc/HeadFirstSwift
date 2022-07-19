bef61c812d8fead6db2cc05d510dee95c1dcef14
Jae Cho <mobilewiz@gmail.com>
2022-07-19 11:22:40 -0400

    Initial Commit

---
+++ HeadFirstSwift/HeadFirstSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadFirstSwiftApp.swift
+//  HeadFirstSwift
+//
+//  Created by Jae Cho on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct HeadFirstSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
